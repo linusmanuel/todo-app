566e1a41016c47b3a5d313d0c8fcab185d0c30b2
Joao Lino <linusmannuel@gmail.com>
2024-06-08 13:09:03 +0100

    refator: add breeze base struture on create page

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,14 +1,17 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight btn btn-info">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Todo App</div>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Todo App</div>
-
-                <div class="card-body">
-                    @if ($errors->any())
+                    <div class="card-body">
+                        @if ($errors->any())
                         <div class="alert alert-danger">
                             <ul>
                                 @foreach ($errors->all() as $error)
@@ -16,25 +19,25 @@
                                 @endforeach
                             </ul>
                         </div>
-                    @endif
+                        @endif
 
-                    <form method="post" action="{{ route('todos.store') }}">
-                        @csrf
-                        <div class="mb-3">
-                            <label class="form-label">Nome da tarefa</label>
-                            <input type="text" class="form-control" name="title" placeholder="Informe o nome da tarefa">
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Descrição</label>
-                            <textarea class="form-control" name="description" cols="5" rows="5"></textarea>
-                        </div>
-                        <div class="col-auto">
-                            <button type="submit" class="btn btn-primary mb-3">Criar</button>
-                        </div>
-                    </form>
+                        <form method="post" action="{{ route('todos.store') }}">
+                            @csrf
+                            <div class="mb-3">
+                                <label class="form-label">Nome da tarefa</label>
+                                <input type="text" class="form-control" name="title" placeholder="Informe o nome da tarefa">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Descrição</label>
+                                <textarea class="form-control" name="description" cols="5" rows="5"></textarea>
+                            </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-primary mb-3">Criar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</x-app-layout>
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="container">
-        <div class="row justify-content-center group">
+        <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header bg-slate-500">{{ __('Dashboard') }}</div>
