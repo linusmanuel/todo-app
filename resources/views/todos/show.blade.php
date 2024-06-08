a7219da49a1762fbbdc85e1473f9f41be635263f
Joao Lino <linusmannuel@gmail.com>
2024-06-08 12:58:02 +0100

    refator: add breeze base struture on show page

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,24 +1,27 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Todo App</div>
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight btn btn-info">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Todo App</div>
+                    <div class="card-body">
+                        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+                        <a href="{{ url()->previous() }}" class="btn btn-sm btn-info">Voltar</a>
+                        <h4>Ver tarefa</h4>
+                        Nome da tarefa: {{ $todo->title }}
+                        Descrição da tarefa: {{ $todo->description }}
                     </div>
-                    @endif
-                    <a href="{{ url()->previous() }}" class="btn btn-sm btn-info">Voltar</a>
-                    <h4>Ver tarefa</h4>
-                    Nome da tarefa: {{ $todo->title }}
-                    Descrição da tarefa: {{ $todo->description }}
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,30 @@
 <?php
 
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
+use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::prefix('todos')->as('todos.')->controller(TodoController::class)->group(function() {
-    Route::get('index', 'Index')->name('index');
-    Route::get('criar', 'Create')->name('create');
-    Route::post('criar', 'Store')->name('store');
-    Route::get('ver/{id}', 'Show')->name('show');
-    Route::get('{id}/editar', 'Edit')->name('edit');
-    Route::put('atualizar', 'update')->name('update');
-    Route::delete('deletar', 'destroy')->name('destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::prefix('todos')->as('todos.')->controller(TodoController::class)->group(function () {
+        Route::get('index', 'Index')->name('index');
+        Route::get('criar', 'Create')->name('create');
+        Route::post('criar', 'Store')->name('store');
+        Route::get('ver/{id}', 'Show')->name('show');
+        Route::get('{id}/editar', 'Edit')->name('edit');
+        Route::put('atualizar', 'update')->name('update');
+        Route::delete('deletar', 'destroy')->name('destroy');
+    });
 });
+
+require __DIR__.'/auth.php';
