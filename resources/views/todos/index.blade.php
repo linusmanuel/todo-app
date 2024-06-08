0ac43c808e2ade3a6c4a660be8c477732eff3d8e
Joao Lino <linusmannuel@gmail.com>
2024-06-09 00:31:49 +0100

    feat: add component button

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,71 +1,57 @@
 <x-app-layout>
     <div class="container">
-        <div class="row justify-content-center group">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        @if(Session::has('alert-success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ Session::get('alert-success') }}
-                        </div>
-                        @endif
+        @if(Session::has('alert-success'))
+        <div class="alert alert-success" role="alert">
+            {{ Session::get('alert-success') }}
+        </div>
+        @endif
 
-                        @if(Session::has('error'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ Session::get('error') }}
-                        </div>
-                        @endif
+        @if(Session::has('error'))
+        <div class="alert alert-danger" role="alert">
+            {{ Session::get('error') }}
+        </div>
+        @endif
 
-                        @if(Session::has('alert-info'))
-                        <div class="alert alert-info" role="alert">
-                            {{ Session::get('alert-info') }}
-                        </div>
-                        @endif
-                        <a href="{{ route('todos.create') }}" class="btn btn-info">Criar nova tarefa</a>
-                        @if (count($todos) > 0)
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Tarefa</th>
-                                    <th scope="col">Descrição</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Ação</th>
-                                    oi
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse($todos as $todo)
-                                <tr>
-                                    <td>{{$todo->title}}</td>
-                                    <td>{{$todo->description}}</td>
-                                    <td>
-                                        @if ($todo->is_completed == 1)
-                                        <a class="btn btn-sm btn-success" href="">Completed</a>
-                                        @else
-                                        <a class="btn btn-sm btn-danger" href="">in Completed</a>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <a class="inner btn btn-sm btn-success" href="{{route('todos.edit', $todo->id)}}">Edit</a>
-                                        <a class="inner btn btn-sm btn-info" href="{{route('todos.show', $todo->id)}}">Ver</a>
-                                        <form method="post" action="{{ route('todos.destroy') }}">
-                                            @csrf
-                                            @method('DELETE')
-                                            <input type="hidden" name="todo_id" value="{{ $todo->id }}">
-                                            <input type="submit" class="btn btn-sm btn-danger" value="Delete">
-                                        </form>
-                                    </td>
-                                </tr>
-                                @empty
-                                @endforelse
-                            </tbody>
-                        </table>
-                        @else
-                        <td>Nothing to display</td>
-                        @endif
-                    </div>
+        @if(Session::has('alert-info'))
+        <div class="alert alert-info" role="alert">
+            {{ Session::get('alert-info') }}
+        </div>
+        @endif
+
+        <h1>Gerencie suas tarefas</h1>
+        <p>Esteja organizado...</p>
+
+        <div class="cards">
+            @if (count($todos) > 0)
+            <div class="card">
+                @forelse($todos as $todo)
+                <div class="card__actions">
+                    <a class="btn btn--update" href="{{route('todos.edit', $todo->id)}}"></a>
+                    <a class="btn btn--show" href="{{route('todos.show', $todo->id)}}"></a>
+                    <form method="post" action="{{ route('todos.destroy') }}">
+                        @csrf
+                        @method('DELETE')
+                        <input type="hidden" name="todo_id" value="{{ $todo->id }}">
+                        <button class="btn btn--delete"></button>
+                    </form>
                 </div>
+                <h2 class="card__title">
+                    {{$todo->title}}
+                </h2>
+                <div class="card__status">
+                    @if ($todo->is_completed == 1)
+                    <a class="" href="">Completed</a>
+                    @else
+                    <a class="" href="">in Completed</a>
+                    @endif
+                </div>
+                @empty
+                @endforelse
             </div>
+            @else
+            <p>Não existem tarefas criadas</p>
+            <a href="{{ route('todos.create') }}" class="btn btn-info">Criar nova tarefa</a>
+            @endif
         </div>
     </div>
 </x-app-layout>
