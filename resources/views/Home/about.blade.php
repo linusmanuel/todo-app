35538a0e900bbceec2868d5fdf24af0f8d66e828
Joao Lino <linusmannuel@gmail.com>
2024-09-29 15:11:55 +0100

    feat: add about video on home page

--- resources/views/Home/about.blade.php
+++ resources/views/Home/about.blade.php
@@ -8,7 +8,7 @@
             </p>
         </header>
         <div class="ilustration-wrapper">
-            <iframe width="1280" height="720" src="https://www.youtube.com/embed/xO7unjpR5ZA" title="AFONSO PADILHA - FILA DE PIADAS #2017" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/BzCIQ9neuos?si=rd7sJx2kQBRCrxZl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     </div>
 </section>
